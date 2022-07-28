d09657b330dffaa6f3f9af834263554754214fa5
Tiger Wang <tigerwang@outlook.com>
2022-07-28 00:10:24 +0000

    wip

--- common/management.go
+++ common/management.go
@@ -2,14 +2,20 @@
 package common
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"path"
 	"path/filepath"
 )
 
 const MANAGEMENT_URL_FILENAME = "management.url"
 const GATEWAY_URL_FILENAME = "gateway.url"
+
+const API_PATH = "/v1/routes"
 
 type Route struct {
 	Path   string `json:"path" binding:"required"`
@@ -23,8 +29,23 @@ type managementService struct {
 	address string
 }
 
-func (m *managementService) CreateRoute(route *Route) {
+func (m *managementService) CreateRoute(route *Route) error {
+	url := path.Join(m.address, API_PATH)
+	body, err := json.Marshal(route)
+	if err != nil {
+		return err
+	}
 
+	response, err := http.Post(url, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode != http.StatusCreated {
+		return errors.New("failed to create route (status code: " + fmt.Sprint(response.StatusCode) + ")")
+	}
+
+	return nil
 }
 
 func NewManagementService(runtimeVariablesPath string) (ManagementService, error) {
