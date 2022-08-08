3739e3bc725dcd18b4447c2d0d02f28e758d7190
Tiger Wang <tigerwang@outlook.com>
2022-08-08 22:21:26 +0000

    wip

--- build/usr/share/casaos/setup/script.d/01-setup-gateway.sh
+++ build/usr/share/casaos/setup/script.d/01-setup-gateway.sh
@@ -1,4 +1,46 @@
 #!/bin/bash
 
+APP_NAME_SHORT=gateway
+
+__get_setup_script_directory_by_os_release() {
+	pushd "$(dirname "${BASH_SOURCE[0]}")/../service.d/${APP_NAME_SHORT}" >/dev/null
+
+	{
+		# shellcheck source=/dev/null
+		{
+			source /etc/os-release
+			{
+				pushd "${ID}"/"${VERSION_CODENAME}" >/dev/null
+			} || {
+				pushd "${ID}" >/dev/null
+			} || {
+				pushd "${ID_LIKE}" >/dev/null
+			} || {
+				echo "Unsupported OS: ${ID} ${VERSION_CODENAME} (${ID_LIKE})"
+				exit 1
+			}
+
+			pwd
+
+			popd >/dev/null
+
+		} || {
+			echo "Unsupported OS: unknown"
+			exit 1
+		}
+
+	}
+
+	popd >/dev/null
+}
+
 set -e
 
+SETUP_SCRIPT_DIRECTORY="$(__get_setup_script_directory_by_os_release)"
+SETUP_SCRIPT_FILENAME="setup-${APP_NAME_SHORT}.sh"
+
+SETUP_SCRIPT_FILEPATH="${SETUP_SCRIPT_DIRECTORY}/${SETUP_SCRIPT_FILENAME}"
+
+echo "✅ Running ${SETUP_SCRIPT_FILEPATH}..."
+
+$SHELL "${SETUP_SCRIPT_FILEPATH}" | sed  "s/^/[${SETUP_SCRIPT_FILENAME}] /"
---
+++ build/usr/share/casaos/setup/service.d/gateway/debian/setup-gateway.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Installing ${APP_NAME_SHORT}..."
+
