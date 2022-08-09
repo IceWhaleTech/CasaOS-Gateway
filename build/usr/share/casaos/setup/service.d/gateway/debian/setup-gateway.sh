63e1e2d6d139cfe26c68f99cce2aa38379848994
Tiger Wang <tigerwang@outlook.com>
2022-08-09 00:32:16 +0000

    wip

--- build/usr/share/casaos/setup/service.d/gateway/debian/setup-gateway.sh
+++ build/usr/share/casaos/setup/service.d/gateway/debian/setup-gateway.sh
@@ -7,9 +7,18 @@ INSTALL_ROOT=${1:-/}
 APP_NAME="casaos-gateway"
 APP_NAME_SHORT="gateway"
 
+BIN_PATH=${INSTALL_ROOT}/usr/bin
+BIN_FILE=${BIN_PATH}/${APP_NAME}
+
 CONF_PATH=${INSTALL_ROOT}/etc/casaos
 CONF_FILE=${CONF_PATH}/${APP_NAME_SHORT}.ini
 CONF_FILE_SAMPLE=${CONF_PATH}/${APP_NAME_SHORT}.ini.sample
+
+VERSION="$($BIN_FILE -v)"
+VERSION_PATH=${INSTALL_ROOT}/var/lib/casaos
+VERSION_FILE=${VERSION_PATH}/${APP_NAME_SHORT}-version
+
+echo "${VERSION}" | tee "${VERSION_FILE}"
 
 if [ ! -f "${CONF_FILE}" ]; then \
     echo "Initializing config file..."
