7904e76bb59021563b8ad28349f2110ac0e1b7ca
Tiger Wang (王豫) <tigerwang@outlook.com>
2022-08-15 18:58:00 -0400

    add incrementation migration logic (#4)

--- build/scripts/setup/service.d/gateway/debian/setup-gateway.sh
+++ build/scripts/setup/service.d/gateway/debian/setup-gateway.sh
@@ -2,29 +2,22 @@
 
 set -e
 
-INSTALL_ROOT=${1:-/}
-
+## base variables
 APP_NAME="casaos-gateway"
 APP_NAME_SHORT="gateway"
 
-BIN_PATH=${INSTALL_ROOT}/usr/bin
-BIN_FILE=${BIN_PATH}/${APP_NAME}
-
-CONF_PATH=${INSTALL_ROOT}/etc/casaos
+# copy config files
+CONF_PATH=/etc/casaos
 CONF_FILE=${CONF_PATH}/${APP_NAME_SHORT}.ini
 CONF_FILE_SAMPLE=${CONF_PATH}/${APP_NAME_SHORT}.ini.sample
-
-VERSION="$($BIN_FILE -v)"
-VERSION_PATH=${INSTALL_ROOT}/var/lib/casaos
-VERSION_FILE=${VERSION_PATH}/${APP_NAME_SHORT}-version
-
-echo "Writing version number '${VERSION}' to ${VERSION_FILE}..."
-echo "${VERSION}" > "${VERSION_FILE}"
 
 if [ ! -f "${CONF_FILE}" ]; then \
     echo "Initializing config file..."
     cp -v "${CONF_FILE_SAMPLE}" "${CONF_FILE}"; \
 fi
+
+# enable and start service
+systemctl daemon-reload
 
 echo "Enabling service..."
 systemctl enable --force --no-ask-password "${APP_NAME}.service"
--- build/usr/share/casaos/migration/script.d/01-migrate-gateway.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-__get_existing_version() {
-    local version
-    
-    echo "$version"
-}
