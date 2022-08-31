24de4e8ffd4eb2394aab19a8d73454dc0ad54dc4
Tiger Wang <tigerwang@outlook.com>
2022-08-31 04:26:29 +0000

    wip

--- build/scripts/migration/script.d/01-migrate-gateway.sh
+++ build/scripts/migration/script.d/01-migrate-gateway.sh
@@ -87,7 +87,12 @@ if [ "${NEED_MIGRATION}" = "false" ]; then
     exit 0
 fi
 
-MIGRATION_SERVICE_DIR=${BUILD_PATH}/scripts/migration/service.d/${APP_NAME_SHORT}
+MIGRATION_SERVICE_DIR=${1}
+
+if [ -z "${MIGRATION_SERVICE_DIR}" ]; then
+    MIGRATION_SERVICE_DIR=${BUILD_PATH}/scripts/migration/service.d/${APP_NAME_SHORT}
+fi
+
 MIGRATION_LIST_FILE=${MIGRATION_SERVICE_DIR}/migration.list
 MIGRATION_PATH=()
 
@@ -143,6 +148,13 @@ pushd "${MIGRATION_SERVICE_DIR}"
 
 {
     for VER2 in "${MIGRATION_PATH[@]}"; do
+        MIGRATION_TOOL_FILE=linux-"${ARCH}"-"${APP_NAME}"-migration-tool-"${VER2}".tar.gz
+
+        if [ -f "${MIGRATION_TOOL_FILE}" ]; then
+            __info "Migration tool ${MIGRATION_TOOL_FILE} exists. Skip downloading."
+            continue
+        fi
+
         MIGRATION_TOOL_URL=https://github.com/IceWhaleTech/"${APP_NAME_FORMAL}"/releases/download/"${VER2}"/linux-"${ARCH}"-"${APP_NAME}"-migration-tool-"${VER2}".tar.gz
         __info "Dowloading ${MIGRATION_TOOL_URL}..."
         curl -sL -O "${MIGRATION_TOOL_URL}"
@@ -157,9 +169,6 @@ pushd "${MIGRATION_SERVICE_DIR}"
         MIGRATION_TOOL_FILE=linux-"${ARCH}"-"${APP_NAME}"-migration-tool-"${VER2}".tar.gz
         __info "Extracting ${MIGRATION_TOOL_FILE}..."
         tar zxvf "${MIGRATION_TOOL_FILE}" || __error "Failed to extract ${MIGRATION_TOOL_FILE}"
-
-        MIGRATION_SYSROOT_DIR=$(realpath -e "${MIGRATION_SERVICE_DIR}"/build/sysroot || __error "Failed to find sysroot directory for migration")
-        cp -rv "${MIGRATION_SYSROOT_DIR}"/* / || __error "Failed to copy sysroot directory for migration"
 
         MIGRATION_TOOL_PATH=build/sysroot/usr/bin/${APP_NAME}-migration-tool
         __info "Running ${MIGRATION_TOOL_PATH}..."
