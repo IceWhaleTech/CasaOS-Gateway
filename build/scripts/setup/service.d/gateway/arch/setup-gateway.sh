3996617d0b1e5cab1018e2b035c08a9888a0c252
Ns2Kracy <2220496937@qq.com>
2022-11-24 11:05:07 +0800

    arch_support

---
+++ build/scripts/setup/service.d/gateway/arch/setup-gateway.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+## base variables
+readonly APP_NAME="casaos-gateway"
+readonly APP_NAME_SHORT="gateway"
+
+# copy config files
+readonly CONF_PATH=/etc/casaos
+readonly CONF_FILE=${CONF_PATH}/${APP_NAME_SHORT}.ini
+readonly CONF_FILE_SAMPLE=${CONF_PATH}/${APP_NAME_SHORT}.ini.sample
+
+if [ ! -f "${CONF_FILE}" ]; then \
+    echo "Initializing config file..."
+    cp -v "${CONF_FILE_SAMPLE}" "${CONF_FILE}"; \
+fi
+
+# enable and start service
+systemctl daemon-reload
+
+echo "Enabling service..."
+systemctl enable --force --no-ask-password "${APP_NAME}.service"
+
+#echo "Starting service..."
+#systemctl start --force --no-ask-password "${APP_NAME}.service"
