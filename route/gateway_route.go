7506d3487a2ae4ffbf59df7d9c3d3e823a1bd3c6
CorrectRoadH <a778917369@gmail.com>
2023-07-24 14:14:23 +0800

    a elegant fix

--- route/gateway_route.go
+++ route/gateway_route.go
@@ -30,12 +30,12 @@ func rewriteRequestSourceIP(r *http.Request) {
 	// `X-Forwarded-For:[::1,::1]`(normal or hacked) attacker can build the request to bypass the verification.
 	// But in the case. the remoteAddress should be the real ip. So we can use remoteAddress to verify it.
 
-	ipList := strings.Split(r.Header.Get("X-Forwarded-For"), ",")
+	ipList := []string{}
 
-	// when r.Header.Get("X-Forwarded-For") is "". to clean the ipList.
+	// when r.Header.Get("X-Forwarded-For") is "". the ipList should be empty.
 	// fix https://github.com/IceWhaleTech/CasaOS/issues/1247
-	if len(ipList) == 1 && ipList[0] == "" {
-		ipList = []string{}
+	if r.Header.Get("X-Forwarded-For") != "" {
+		ipList = strings.Split(r.Header.Get("X-Forwarded-For"), ",")
 	}
 
 	r.Header.Del("X-Forwarded-For")
