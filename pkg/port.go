629dc17c62d6ba62556dedc23f63e4c256f2870b
CorrectRoadH <a778917369@gmail.com>
2024-07-12 16:13:42 +0800

    wip

--- pkg/port.go
+++ pkg/port.go
@@ -2,20 +2,11 @@ package pkg
 
 import (
 	"errors"
-	"fmt"
-	"os"
-	"path/filepath"
 
-	"github.com/IceWhaleTech/CasaOS-Common/utils/constants"
 	"github.com/IceWhaleTech/CasaOS-Gateway/common"
 )
 
 func GetGatewayPort() (int, error) {
-	ConfigFilePath := filepath.Join(constants.DefaultConfigPath, common.GatewayName+"."+common.GatewayConfigType)
-	if _, err := os.Stat(ConfigFilePath); os.IsNotExist(err) {
-		return 0, errors.New(fmt.Sprintf("config file %s not exist", ConfigFilePath))
-	}
-
 	config, err := common.LoadConfig()
 	if err != nil {
 		return 0, err
